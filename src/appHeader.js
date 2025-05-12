df102df45d2afaea12b3d3bb2959c5f53e137954
dotcom <dotcom29102001@gmail.com>
2025-05-12 14:43:09 +0700

    add appHeader.js

---
+++ src/appHeader.js
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
+// การสร้าง Component
+
+function AppHeader() {
+    return (
+        <header className="app-header">
+            <img className="app-header-logo" src='/image/logo.jpg' />
+        </header>
+    );
+};
+
+// ส่งออก AppHeader
+export default AppHeader;
