90fc995efbe0d7ae6b560fd9c8e0eb3d69f851c8
dotcom <dotcom29102001@gmail.com>
2025-05-12 14:43:55 +0700

    add TattooItem.js

---
+++ src/TattooItem.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+function TattooItem() {
+    return(
+        <div className="tattoo-item">
+            <img src="/image/tattoo1.jpg" />
+            <h4>ต่ายน้อย 1</h4>
+        </div>
+    );
+};
+
+export default TattooItem;
