f69b8fb388c2006ee422f8cdf2477e3a1ed73ca9
dotcom <dotcom29102001@gmail.com>
2025-05-12 09:46:43 +0700

    updatefile

--- Backend-Basic/Python/PythonBeginner-master/PythonBeginner-master/Phase2/5.FrozenSet.py
+++
@@ -1,7 +0,0 @@
-# FrozenSet ไม่สามารถแก้ไขข้อมูลอะไรได้เลย
-fruit=frozenset(["มะม่วง","มะยม","มะนาว"])
-fruit.add("ทุเรียน")
-fruit.discard("มะยม")
-print(fruit) #eror
-for item in fruit:
-    print("ข้อมูล=>",item) #ปริ้นข้อมูลปต่ละตัว
--- Backend-Basic/server/server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.listen(5000, ()=> console.log('Server is Running port 5000'))
